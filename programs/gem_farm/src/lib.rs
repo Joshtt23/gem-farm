806b2db166bd3eab01c1169ea060cfd6fc0e1188
The Lost Toastr <josht98@aol.com>
2022-08-10 01:17:23 -0400

    new

--- programs/gem_farm/src/lib.rs
+++ programs/gem_farm/src/lib.rs
@@ -7,7 +7,7 @@ pub mod instructions;
 pub mod number128;
 pub mod state;
 
-declare_id!("farmL4xeBFVXJqtfxCzU9b28QACM7E2W2ctT6epAjvE");
+declare_id!("4rvYWYGG3EqnRhrdbgvTMS2bfpPkzZnzLodZBRJ8pJF8");
 
 #[program]
 pub mod gem_farm {
